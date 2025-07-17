edae83026d6ff500a7f35c54e4040e9d6ea12f21
d3bibek <95917570+d3bibek@users.noreply.github.com>
2025-07-17 12:28:58 +0000

    python

---
+++ Day-04/calculator.py
@@ -0,0 +1,15 @@
+def Name(a,b):
+    name=a+" "+b
+    return name
+print(Name("Hi","I'm Shaurya"))
+
+def Age(num1,num2):
+    days=num1+num2
+    return days
+
+print("Age:", Age(30,10), "Days")
+
+def Fingers(l_l_fingers,r_l_fingers,l_hand,r_hand):
+    total=l_l_fingers+r_l_fingers+l_hand+r_hand
+    return total
+print("I have total:", Fingers(5,5,5,5), "Fingers")
